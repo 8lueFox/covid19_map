9db70569bcfac05f98c6859590d96f6888f42c0c
Kacper <kacper.jedrzejewski@int.pl>
2021-06-13 12:32:44 +0200

    [add] country details

--- web/src/Components/CountryDetails.jsx
+++ web/src/Components/CountryDetails.jsx
@@ -3,14 +3,22 @@ import { AddSpaceBeetwenCasesNumber } from './CovidMap'
 
 export const CountryDetails = ({details}) => {
     return (
-        <div className='cloud'>
+        <div className='cloud' id='countryDetails'>
             {details === undefined || details.message !== undefined ? (
                 <p className='notFoundCountry'>Choose country to see details</p>
             ):(
                 <div>
                     <p><b>{details.country} / {details.continent}</b></p>
-                    <img src={details.countryInfo.flag} alt={details.country + "_flag"} style={{width: '40%'}} />
-                    <p><span className='light-text'>Today cases:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.todayCases)}</span></p>
+                    <img id='mapImg' src={details.countryInfo.flag} alt={details.country + "_flag"} />
+                    <p><span className='light-text br'>Today cases:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.todayCases)}</span></p>
+                    <p><span className='light-text br'>Today deaths:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.todayDeaths)}</span></p>
+                    <p><span className='light-text br'>Today recovered:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.todayRecovered)}</span></p>
+                    <hr />
+                    <p><span className='light-text br'>Cases:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.cases)}</span></p>
+                    <p><span className='light-text br'>Deaths:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.deaths)}</span></p>
+                    <p><span className='light-text br'>Recovered:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.recovered)}</span></p>
+                    <p><span className='light-text br'>Tests:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.tests)}</span></p>
+                    <p><span className='light-text br'>Population:</span> <span className='red-text'>{AddSpaceBeetwenCasesNumber(details.population)}</span></p>
                 </div>
             )}
         </div>
